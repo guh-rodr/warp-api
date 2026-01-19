b85fa9e7aeeb4b309dba0b8de8d12ec10aac427c
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-01-19 14:44:56 -0300

    fix: change host to 0.0.0.0 (support for deploy)

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
 
   app.register(fastifyCookie);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
